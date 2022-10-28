ba0b2d879594ae49ef06a9c2afd04fa4a1a7852a
sardar4july <109798571+sardar4july@users.noreply.github.com>
2022-10-28 15:59:39 +0530

    Update requestpractice.py

--- requestpractice.py
+++ requestpractice.py
@@ -29,18 +29,5 @@ import requests
 #     print(r.status_code)
 # except requests.exceptions.RequestException as e:
 #       print(e)
-# import math
-# l=[1,2,3,4,5]
-# x=math.prod(l)
-# new=[x//i for i in l ]
-# print(new)
-l=[5,1,1,5]
-d=[[0 for i in range(2)]for j in range(5)]
-# print(d)
-d[0][0]=0
-d[0][1]=l[0]
-for i in range(1,len(l)):
-    d[i][1]=d[i-1][0]+l[i]
-    d[i][0]=max(d[i-1][1],d[i-1][0])
 
-print((d))
+
