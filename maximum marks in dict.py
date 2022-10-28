ffff2fddb2701c923acd2d77e463bb01438f7771
sardar4july <109798571+sardar4july@users.noreply.github.com>
2022-10-28 15:38:53 +0530

    Add files via upload

---
+++ maximum marks in dict.py
@@ -0,0 +1,45 @@
+# marks={}
+# d={'z':{'m':90,'s':40,'e':65},
+#    'b':{'m':60,'s':95,'e':55},
+#    'c':{'m':90,'s':60,'e':99}}
+
+# for k,v in d.items():
+#    max=0
+#    for m in v.values():
+#       if m>max:
+#          max=m
+#    marks[k]=max
+# marks={k:sum(v.values()) for k,v in d.items()}
+# marks={k:max(v.values()) for k,v in d.items()}
+# print(marks)
+'''total maximum marks'''
+me={}
+# for k,v in d.items():
+#     m[k]=sum(v.values())
+# print(m)
+'''individual highest marks  '''
+# for k,v in d.items():
+#     mx=0
+#     mk=0
+#     for x,y in v.items():
+#         if y>mx:
+#            mx=y
+#            mk=x
+#     me[k]=(mk,mx)
+# print(me)
+
+# test_dict = {'Manjeet' : {'Maths':1, 'English':0, 'Physics':2, 'Chemistry':3},
+#             'Akash' : {'Maths':0, 'English':0, 'Physics':3, 'Chemistry':2},
+#             'Nikhil': {'Maths':4, 'English':2, 'Physics':2, 'Chemistry':3},
+#             'Akshat': {'Maths':1, 'English':0, 'Physics':2, 'Chemistry':0}}
+# from collections import Counter
+# res=[]
+# for k,v in test_dict.items():
+#     cnt=Counter(v)
+#     res.append({k:cnt.most_common(1)[0]})
+#
+# print(res)
+
+
+
+
